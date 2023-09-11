c00b4dd01b7241f34057244723eb288877d6e755
samuel <samuelsbaggio@gmail.com>
2023-09-10 21:05:20 -0300

    commit inicial

---
+++ src/main/java/br/com/fiap/prontuarioback/ProntuarioBackApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.prontuarioback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProntuarioBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProntuarioBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/prontuarioback/ProntuarioBackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.prontuarioback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProntuarioBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
